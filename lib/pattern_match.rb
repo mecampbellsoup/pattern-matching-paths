f2b14b9073d0598550f2758da39f1d6b3a05d5ae
Matt Campbell <mecampbell25@gmail.com>
2015-07-05 21:12:28 -0400

    Open classing Array with #with_fewest_wildcards

--- lib/pattern_match.rb
+++ lib/pattern_match.rb
@@ -1,7 +1,28 @@
 $:.unshift File.dirname(__FILE__)
 
 require 'pattern_match/pattern'
+require 'pattern_match/path'
 require 'pattern_match/runner'
 
 module PatternMatch
+end
+
+class Array
+  def with_fewest_wildcards
+    # identify pattern with fewest wildcards
+    fewest = self.reduce do |memo, pattern|
+      memo.wildcard_count < pattern.wildcard_count ? memo : pattern
+    end
+
+    # check if multiple patterns have same number of wildcards
+    multiple = self.select { |p| p.wildcard_count == fewest.wildcard_count }
+
+    if multiple.size > 1
+      multiple.reduce do |memo, pattern|
+        memo.wildcards.first > pattern.wildcards.first ? memo : pattern
+      end
+    else
+      fewest
+    end
+  end
 end
